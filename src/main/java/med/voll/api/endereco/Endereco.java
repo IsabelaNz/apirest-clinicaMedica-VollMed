44c345d2c8a6c54b63d2c1d50389166b69a153cb
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-03-13 11:42:20 -0300

    Entidades JPA

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,23 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Endereco {
+
+    private String  Logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,32 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*; // Importa as anotações do JPA
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos") // Especifica o nome da tabela no banco de dados
+@Entity(name = "medicos") // Indica que essa classe representa uma entidade JPA, mapeando-a para a tabela "medicos"
+@Getter // Gera automaticamente os métodos getter para todos os atributos
+@NoArgsConstructor // Cria um construtor sem argumentos
+@AllArgsConstructor // Cria um construtor com todos os argumentos
+@EqualsAndHashCode(of = "id") // Gera automaticamente os métodos equals() e hashCode() com base no atributo "id"
+
+public class Medico {
+
+    @Id // Define que este atributo é a chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Configura a estratégia de geração do ID como autoincremento no banco de dados
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING) // Mapeia o enum para ser armazenado como texto (STRING) no banco de dados
+    private Especialidade especialidade;
+
+    @Embedded // Indica que o atributo endereco é um objeto incorporado dentro da entidade Medico, representando um componente da entidade
+    private Endereco endereco;
+}
