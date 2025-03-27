1f021a1a4f33088153496878c0fa952cda909068
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-03-27 19:13:11 -0300

    Testando listagem

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -9,14 +9,14 @@ import med.voll.api.endereco.Endereco;
 
 @Table(name = "medicos") // Especifica o nome da tabela no banco de dados
 @Entity(name = "medicos") // Indica que essa classe representa uma entidade JPA, mapeando-a para a tabela "medicos"
-@Getter // Gera automaticamente os métodos getter para todos os atributos
-@NoArgsConstructor // Cria um construtor sem argumentos
 @AllArgsConstructor // Cria um construtor com todos os argumentos
 @EqualsAndHashCode(of = "id") // Gera automaticamente os métodos equals() e hashCode() com base no atributo "id"
 
 public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // Configura a estratégia de geração do ID como autoincremento no banco de dados
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Configura a estratégia de geração do ID como autoincremento no banco de dados
     private Long id;
     private String nome;
     private String email;
@@ -26,7 +26,8 @@ public class Medico {
     @Enumerated(EnumType.STRING) // Mapeia o enum para ser armazenado como texto (STRING) no banco de dados
     private Especialidade especialidade;
 
-    @Embedded // Indica que o atributo endereco é um objeto incorporado dentro da entidade Medico, representando um componente da entidade
+    @Embedded
+    // Indica que o atributo endereco é um objeto incorporado dentro da entidade Medico, representando um componente da entidade
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
@@ -36,5 +37,42 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public Medico() {
+        // Construtor vazio exigido pelo JPA
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
     }
 }
