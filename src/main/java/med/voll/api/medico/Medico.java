b98d4e738c99d5e0d25a2f8b60570ba9d7a46fe2
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-04-23 13:43:06 -0300

    Atualizando dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*; // Importa as anotações do JPA
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -75,4 +76,22 @@ public class Medico {
     public void setEspecialidade(Especialidade especialidade) {
         this.especialidade = especialidade;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
 }
