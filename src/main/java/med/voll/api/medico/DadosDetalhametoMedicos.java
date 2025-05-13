4da8fc5b887d1ea0147b3ab7c4959684c80552a9
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-05-13 18:32:33 -0300

    Desenvolvendo http 201

---
+++ src/main/java/med/voll/api/medico/DadosDetalhametoMedicos.java
@@ -0,0 +1,25 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosDetalhametoMedicos(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        String telefone,
+        Especialidade especialidade,
+        Endereco endereco
+) {
+    public DadosDetalhametoMedicos(Medico medico) {
+        this(
+                medico.getId(),
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getTelefone(),
+                medico.getEspecialidade(),
+                medico.getEndereco()
+        );
+    }
+}
