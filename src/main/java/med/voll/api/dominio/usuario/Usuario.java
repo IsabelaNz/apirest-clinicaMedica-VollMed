d2dd9d7a473a1a7fbefd63161c9c011d06678fc8
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-05-27 20:33:40 -0300

    Controller de autenticação

--- src/main/java/med/voll/api/dominio/usuario/Usuario.java
+++ src/main/java/med/voll/api/dominio/usuario/Usuario.java
@@ -3,12 +3,18 @@ package med.voll.api.dominio.usuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 //essa classe é uma entidade jpa
 
 @Table(name = "usuarios")
 @Entity(name = "Usuarios")
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +23,38 @@ public class Usuario {
     private String senha;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
